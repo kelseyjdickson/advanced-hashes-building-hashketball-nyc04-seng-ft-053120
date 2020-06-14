89bf72557220013a58d2aa77554098272417330e
Kelsey Dickson <kelseymedved@gmail.com>
2020-06-14 19:58:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,7 +17,7 @@ def game_hash
           blocks: 1,
           slam_dunks: 1
           
-        },
+        
         { player_name: "Reggie Evans",
           number: 30,
           shoe: 14,
@@ -27,7 +27,7 @@ def game_hash
           steals: 12,
           blocks: 12,
           slam_dunks: 7
-        },
+        
         { player_name: "Brook Lopez",
           number: 11,
           shoe: 17,
@@ -37,7 +37,7 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 15
-        },
+        
         { player_name: "Mason Plumlee",
           number: 1,
           shoe: 19,
@@ -47,7 +47,7 @@ def game_hash
           steals: 3,
           blocks: 8,
           slam_dunks: 5
-        },
+        
         } player_name: "Jason Terry",
           number: 31,
           shoe: 15,
@@ -75,7 +75,7 @@ def game_hash
             steals: 2,
             blocks: 7,
             slam_dunks: 2
-          },
+          
          {  player_name: "Bismack Biyombo",
             number: 0,
             shoe: 16,
@@ -85,7 +85,7 @@ def game_hash
             steals: 22,
             blocks: 15,
             slam_dunks: 10 
-          },
+          
           {   player_name: "DeSagna Diop",
               number: 2,
               shoe: 14,
@@ -95,7 +95,7 @@ def game_hash
               steals: 4,
               blocks: 5,
               slam_dunks: 5 
-            },
+            
             {  player_name: "Ben Gordon",
                 number: 8,
                 shoe: 15,
@@ -105,7 +105,7 @@ def game_hash
                 steals: 1,
                 blocks: 1,
                 slam_dunks: 0 
-              },
+              
               { player_name: "Kemba Walker",
                 number: 33,
                 shoe: 15,
