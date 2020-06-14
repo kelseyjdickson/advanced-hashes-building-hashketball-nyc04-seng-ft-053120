a29bd8425037f9ce045b90b6d4d76eda54aec72f
Kelsey Dickson <kelseymedved@gmail.com>
2020-06-14 19:52:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -51,7 +51,7 @@ def game_hash
           blocks: 8,
           slam_dunks: 5
         },
-        "Jason Terry" => {
+         {
           player_name: "Jason Terry",
           number: 31,
           shoe: 15,
